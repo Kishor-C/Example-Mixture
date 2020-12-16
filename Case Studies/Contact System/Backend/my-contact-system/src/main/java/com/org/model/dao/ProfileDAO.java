c6147f2284e7fe203b81ac9fa03405d6ae956695
Kishor <kishor1124@gmail.com>
2020-12-16 17:28:12 +0530

    deleted some files cms

--- Case Studies/Contact System/Backend/my-contact-system/src/main/java/com/org/model/dao/ProfileDAO.java
+++
@@ -1,6 +0,0 @@
-package com.org.model.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.org.model.beans.Profile;
-
-public interface ProfileDAO extends JpaRepository<Profile, Integer>{}
